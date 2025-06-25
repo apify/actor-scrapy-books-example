1cc50bac9c6fa8a3d41069a0d7f43be18ae173ff
Vlada Dusek <v.dusek96@gmail.com>
2025-06-25 10:35:26 +0200

    chore: migrate to uv (#9)

--- books_scraper/settings.py
+++ books_scraper/settings.py
@@ -1,20 +1,20 @@
-BOT_NAME = 'books_scraper'
-LOG_LEVEL = 'INFO'
+BOT_NAME = "books_scraper"
+LOG_LEVEL = "INFO"
 
 # Set depth limit to 50 since there are 50 pages of books on the books.toscrape.com
 DEPTH_LIMIT = 50
 
-SPIDER_MODULES = ['books_scraper.spiders']
-NEWSPIDER_MODULE = 'books_scraper.spiders'
+SPIDER_MODULES = ["books_scraper.spiders"]
+NEWSPIDER_MODULE = "books_scraper.spiders"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Set settings whose default value is deprecated to a future-proof value
-REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
-TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
-FEED_EXPORT_ENCODING = 'utf-8'
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
 
 ITEM_PIPELINES = {
-    'books_scraper.pipelines.CleaningPipeline': 100,
+    "books_scraper.pipelines.CleaningPipeline": 100,
 }
